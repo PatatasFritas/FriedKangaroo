d798e3bcb146ce2bddb2380c72a42bd5a3076539
Jesus Anton <git@jesux.es>
2020-05-27 22:00:18 +0200

    Add Workfile export Option

--- Kangaroo.h
+++ Kangaroo.h
@@ -119,6 +119,7 @@ public:
   void Check(std::vector<int> gpuId,std::vector<int> gridSize);
   void MergeWork(std::string &file1,std::string &file2,std::string &dest);
   void WorkInfo(std::string &fileName);
+  void WorkExport(std::string &fileName);
 
   // Threaded procedures
   void SolveKeyCPU(TH_PARAM *p);
